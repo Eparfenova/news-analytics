bd529322690636c31b09c8dd12c1e6268e5742ac
Polivanov.Maksim <mpolivanov@bivgroup.com>
2020-07-05 19:33:44 +0300

    update constants

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,5 +1,5 @@
 export const NEWS_API_KEY = "5ec581f3ec334307ac4c68f31dfcca51";
-export const NEWS_API_URL =  "https://newsapi.org/v2/everything";
+export const NEWS_API_URL =  "https://praktikum.tk/news/v2/everything";
 export const NEWS_API_DATE_LAG = 7;
 export const NEWS_FORMAT_DATE = 'YYYY-MM-DD';
 export const GITHUB_OWNER = "Eparfenova"
